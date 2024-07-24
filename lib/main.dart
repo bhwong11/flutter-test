01b31556f82fbe863ffcaa0ae115fa8333f4be2a
Bryant Wong <bryanthwong@gmail.com>
2024-07-24 19:13:56 -0400

    added print

--- lib/main.dart
+++ lib/main.dart
@@ -137,6 +137,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    print('rebuilding');
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
